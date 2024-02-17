2321553b7e394b6d830bbc43a24565fe0d58015b
mats-werrebrouck <mats-werrebrouck@outlook.be>
2024-02-17 16:33:02 +0100

    Implement binary search

---
+++ test/search_algorithms/test_binary_search.cpp
@@ -0,0 +1,14 @@
+#include "../catch.hpp"
+#include "../../src/search_algorithms/binary_search.cpp"
+
+TEST_CASE("binary search") {
+    std::vector<int> arr = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
+
+    SECTION("target is in the array") {
+        REQUIRE(binary_search(arr, 7) == 3);
+    }
+
+    SECTION("target is not in the array") {
+        REQUIRE(binary_search(arr, 8) == -1);
+    }
+}
